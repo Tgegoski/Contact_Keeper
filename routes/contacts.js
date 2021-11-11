1a513ca525bd21890c9ecd71bec625096cfee6c1
Tammy Gegoski <tgegoski@gmail.com>
2021-11-10 19:04:39 -0500

    added auth to contacts post route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
 const Contact = require('../models/Contact');
@@ -25,8 +25,40 @@ router.get('/', auth, async (req, res) => {
 // @desc    Add new contact
 // @access  Private
 
-router.post('/', (req, res) => {
-    res.send('Add contact');
+router.post('/', 
+[
+    auth, 
+    [
+        check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+],
+ async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, phone, type} = req.body;
+
+    try {
+        const newContact = new Contact({
+            name, 
+            email, 
+            phone, 
+            type, 
+            user: req.user.id
+        });
+
+        const contact = await newContact.save();
+
+        res.json(contact);
+    } catch (err) {
+        console.error(er.message);
+        res.status(500).send('Server Error');
+
+    }
+    // res.send('Add contact');
 });
 
 // @route   PUT api/contacts/:id
