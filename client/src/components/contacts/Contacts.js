4c1b9ad72e7096efc56c61c58de0a361161d2ce7
Tammy Gegoski <tgegoski@gmail.com>
2021-11-16 23:06:24 -0500

    set up ContactItem and brought into Contacts.js

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,14 +1,17 @@
 import React, { Fragment, useContext } from 'react';
+import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
-export const Contacts = () => {
+const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
     const { contacts } = contactContext;
 
     return (
         <Fragment>
-            {contacts.map(contact => <h3>{contact.name}</h3>)}
+            {contacts.map(contact => ( 
+                <ContactItem contact={contact} />
+            ))}
         </Fragment>
     );
 };
