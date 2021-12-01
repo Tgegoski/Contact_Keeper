c2f40b103aa0d2cc11661b185148c42e1520aa45
Tammy Gegoski <tgegoski@gmail.com>
2021-11-30 22:16:50 -0500

    added CSS Transition fade to buttons

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,25 +1,32 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
     const contactContext = useContext(ContactContext);
 
+    const { contacts, filtered } = contactContext;
+
     if(contacts.length === 0) {
         return <h4>Please add a contact</h4>
     }
 
-    const { contacts, filtered } = contactContext;
-
     return (
         <Fragment>
-            {filtered !== null ? filtered.map(contact => (
-                <ContactItem key={contact.id}
-            contact={contact} />)) 
+            <TransitionGroup>
+            {filtered !== null 
+            ? filtered.map(contact => (
+                <CSSTransition key={contact.id} timeout={500} className='item'>
+                <ContactItem   contact={contact} />
+                </CSSTransition>
+                )) 
             : contacts.map(contact => (
-                <ContactItem k={contact.id} contact={contact} />
+            <CSSTransition key={contact.id} timeout={500} className='item'>   
+                <ContactItem contact={contact} />
+                </CSSTransition>
             ))}
-            
+            </TransitionGroup> 
         </Fragment>
     );
 };
