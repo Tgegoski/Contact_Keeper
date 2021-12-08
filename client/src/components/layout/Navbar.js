2ffa1e55bcde2ddedd2401da43699414d400aba1
Tammy Gegoski <tgegoski@gmail.com>
2021-12-07 21:02:36 -0500

    added Logout to AuthState and AuthReducer and Logout and onLogout to NavBar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,12 +8,18 @@ const Navbar = ({ title, icon }) => {
 
     const { isAuthenticated, logout, user } = authContext;
 
+    const onLogout = () => {
+        logout();
+    };
+
     const authLinks = (
         <Fragment>
             <li>Hello { user && user.name }</li>
             <li>
-                <a href='#!'></a>
-                <i className='fas fa-sign-out-alt'></i> <span className='hide-sm'>Logout</span>
+                <a onClick={onLogout} href='#!'>
+                <i className='fas fa-sign-out-alt'></i>
+                <span className='hide-sm'>Logout</span>
+            </a>
             </li>
         </Fragment>
     );
