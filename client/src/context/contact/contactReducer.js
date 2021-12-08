086d19b7390ca0a1e38b52f8f2512fd31a22c51c
Tammy Gegoski <tgegoski@gmail.com>
2021-12-08 00:12:58 -0500

    added _ to contact.id key to sync with mongoDB

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -20,14 +20,14 @@ export default (state, action) => {
       return {
         ...state,
         contacts: state.contacts.map((contact) =>
-          contact.id === action.payload.id ? action.payload : contact
+          contact._id === action.payload.id ? action.payload : contact
         ),
       };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
-          (contact) => contact.id !== action.payload
+          (contact) => contact._id !== action.payload
         ),
       };
     case SET_CURRENT:
