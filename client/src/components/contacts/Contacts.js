7dee279dff5b7bbfe62aebaf90060cc0798778cd
Tammy Gegoski <tgegoski@gmail.com>
2021-12-21 22:27:08 -0500

    refactored Contacts.js using state and dispatch instead of context

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,27 +1,25 @@
-import React, { Fragment, useContext, useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import Spinner from '../layout/Spinner';
-import ContactContext from '../../context/contact/contactContext';
+import { useContacts, getContacts } from '../../context/contact/ContactState';
 
 const Contacts = () => {
-  const contactContext = useContext(ContactContext);
+  const [contactState, contactDispatch] = useContacts();
 
-  const { contacts, filtered, getContacts, loading } = contactContext;
+  const { contacts, filtered } = contactState;
 
   useEffect(() => {
-    getContacts();
-    // eslint-disable-next-line
-  }, []);
+    getContacts(contactDispatch);
+  }, [contactDispatch]);
 
-  if (contacts !== null && contacts.length === 0 && !loading) {
+  if (contacts !== null && contacts.length === 0)  {
     return <h4>Please add a contact</h4>;
   }
-  if (loading) return <Spinner />;
 
   return (
     <Fragment>
-      {contacts !== null && !loading ? (
+      {contacts !== null ? (
         <TransitionGroup>
           {filtered !== null
             ? filtered.map((contact) => (
