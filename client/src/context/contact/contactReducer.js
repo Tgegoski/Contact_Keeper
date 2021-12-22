d6fc21a339e86bb86e9f5eac8f4d3fa009e71d4f
Tammy Gegoski <tgegoski@gmail.com>
2021-12-21 23:11:05 -0500

    refactored contactReducer to remove loading and add export default contactReducer

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -11,75 +11,72 @@ import {
   CLEAR_CONTACTS,
 } from '../types';
 
-export default (state, action) => {
+const contactReducer = (state, action) => {
   switch (action.type) {
     case GET_CONTACTS:
       return {
         ...state,
         contacts: action.payload,
-        loading: false,
       };
     case ADD_CONTACT:
       return {
         ...state,
         contacts: [action.payload, ...state.contacts],
-        loading: false,
-      };
+        };
     case UPDATE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.map((contact) =>
           contact._id === action.payload._id ? action.payload : contact
-        ),
-        loading: false,
-      };
+        )
+        };
     case DELETE_CONTACT:
       return {
         ...state,
         contacts: state.contacts.filter(
           (contact) => contact._id !== action.payload
-        ),
-        loading: false,
-      };
+        )
+        };
     case CLEAR_CONTACTS:
       return {
         ...state,
         contacts: null,
         filtered: null,
         error: null,
-        current: null,
+        current: null
       };
     case SET_CURRENT:
       return {
         ...state,
-        current: action.payload,
+        current: action.payload
       };
     case CLEAR_CURRENT:
       return {
         ...state,
-        current: null,
+        current: null
       };
 
     case FILTER_CONTACTS:
       return {
         ...state,
-        filtered: state.contacts.filter((contact) => {
-          const regex = new RegExp(`${action.payload}`, 'gi');
-          return contact.name.match(regex) || contact.email.match(regex);
-        }),
+        filtered: state.contacts.filter((name, email) => {
+          const testString = `${name}${email}`.toLowerCase();
+          return testString.includes(action.payload.toLowerCase());
+        })
       };
     case CLEAR_FILTER:
       return {
         ...state,
-        filtered: null,
-        loading: false,
+        filtered: null
       };
     case CONTACT_ERROR:
       return {
         ...state,
-        error: action.payload,
+        error: action.payload
       };
     default:
-      return state;
+      throw new Error(`Unsupported type of: ${action.type}`);
   }
 };
+
+export default contactReducer;
