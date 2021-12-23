1c8ab99c57487e5d94a0aa11b352d968d762409f
Tammy Gegoski <tgegoski@gmail.com>
2021-12-22 21:31:42 -0500

    refactored ContactFilter using dispatch and onSubmit

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,30 +1,20 @@
-import React, { useContext, useRef, useEffect } from 'react';
-import ContactContext from '../../context/contact/contactContext';
+import React from 'react';
+import { useContacts, filterContacts, clearFilter } from '../../context/contact/ContactState';
 
 const ContactFilter = () => {
-  const contactContext = useContext(ContactContext);
-  const text = useRef('');
+  const contactDispatch = useContacts()[1];
 
-  const { filterContacts, clearFilter, filtered } = contactContext;
-
-  useEffect(() => {
-    if (filtered === null) {
-      text.current.value = '';
-    }
-  });
-
-  const onChange = (e) => {
-    if (text.current.value !== '') {
-      filterContacts(e.target.value);
+   const onChange = (e) => {
+    if (e.target.value !== '') {
+      filterContacts(contactDispatch, e.target.value);
     } else {
-      clearFilter();
+      clearFilter(contactDispatch);
     }
   };
 
   return (
-    <form>
+    <form onSubmit={(e) => e.preventDefault()}>
       <input
-        ref={text}
         type='text'
         placeholder='Filter Contacts...'
         onChange={onChange}
