ee32d46cd70efa19ded1daee8563459260797233
Tammy Gegoski <tgegoski@gmail.com>
2021-12-22 23:48:51 -0500

    cleaned up and changed body to check in user.js routes

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const { body, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -14,9 +14,9 @@ const User = require('../models/User');
 router.post(
   '/',
   [
-    body('name', 'Please add name').not().isEmpty(),
-    body('email', 'Please include a valid email').isEmail(),
-    body(
+    check('name', 'Please add name').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
       'password',
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
@@ -39,7 +39,7 @@ router.post(
       user = new User({
         name,
         email,
-        password,
+        password
       });
 
       const salt = await bcrypt.genSalt(10);
@@ -50,8 +50,8 @@ router.post(
 
       const payload = {
         user: {
-          id: user.id,
-        },
+          id: user.id
+        }
       };
       jwt.sign(
         payload,
